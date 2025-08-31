46ef9cb6cb05edfc6798fad50941dfa29691ee78
jelianyszoe123 <jelianysortiz19713@gmail.com>
2025-08-31 09:46:29 -0400

    ruruurur

--- Script.js
+++ Script.js
@@ -1,3 +1,34 @@
 document.querySelector(".profile img").addEventListener("click", () => {
   alert("¡Hola, soy Salomé!");
 });
+
+
+const player = document.getElementById("player");
+
+function playAudio() {
+  player.play();
+}
+
+function pauseAudio() {
+  player.pause();
+}
+
+function changeTrack(track) {
+  player.src = track;
+  player.load();
+  player.play();
+}
+
+function stopAudio() {
+  player.pause();
+  player.currentTime = 0;
+}
+
+function changeTrack(track) {
+  const player = document.getElementById("player");
+  player.src = track;
+  player.load();
+  player.play();
+}
+
+
